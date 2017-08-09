2c8cb59698a5e14151aa8860c4f3739c04f157e4
Thinus Jacobs <marthinuswesseljacobs@gmail.com>
2017-08-09 18:20:53 +0200

    moqrepo enhancements for in memory storage.

--- c#/RealmdigitalInterview.Core/Implementations/MoqRepo.cs
+++ c#/RealmdigitalInterview.Core/Implementations/MoqRepo.cs
@@ -7,43 +7,70 @@ namespace RealmdigitalInterview.Core.Implementations
 {
     public class MoqRepo : IRepoService
     {
-        private List<object> moqCollection = new List<object>();
+        private static List<object> moqCollection = new List<object>();
 
-        private T InstanceMap<T>(object parameters) {
-            var properties = typeof(T).GetProperties();
-            T instance = Activator.CreateInstance<T>();
-            foreach (var property in properties)
+        private T InstanceMap<T>(object parameters)
+        {
+            if (parameters != null)
             {
-                var name = property.Name;
-                var parName = parameters.GetType().GetProperty(name);
-                if (parName != null)
+                var properties = typeof(T).GetProperties();
+                T instance = Activator.CreateInstance<T>();
+                foreach (var property in properties)
                 {
-                    var value = parName.GetValue(parameters, null);
+                    var name = property.Name;
 
-                    property.SetValue(instance, value, null);
+
+                    var parName = parameters.GetType().GetProperty(name);
+                    if (parName != null)
+                    {
+                        var value = parName.GetValue(parameters, null);
+
+                        property.SetValue(instance, value, null);
+                    }
+
                 }
-            }            
-            return instance;
+                return instance;
+            }
+            return default(T);
         }
 
-        private object FindById(object id) {
+        private object FindById<T>(object id)
+        {
+            var subCollection = moqCollection.OfType<T>().ToList();
+
             var firstProperty = id.GetType().GetProperties().First();
             var idName = firstProperty.Name;
             var idValue = firstProperty.GetValue(id);
-            var item = moqCollection.Single(x => x.GetType().GetProperty(idName).GetValue(x).ToString() == idValue.ToString());
-            return item;
+            if (subCollection != null && subCollection.Count > 0)
+            {
+                var item = subCollection.Where(x => x.GetType().GetProperty(idName).GetValue(x).ToString() == idValue.ToString());
+                return item;
+            }
+            return null;
         }
 
         public T Add<T>(string command, object parameters)
         {
             T instance = InstanceMap<T>(parameters);
+
+            //assume first parameter will always be the primary key and is always an int.
+            //assign incremented primary key
+            try
+            {
+                instance.GetType().GetProperties()[0].SetValue(instance, moqCollection.Count + 1);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
             moqCollection.Add(instance);
             return instance;
         }
 
         public T Delete<T>(string command, object id)
         {
-            var item = FindById(id);
+            var item = FindById<T>(id);
             var index = moqCollection.IndexOf(item);
             moqCollection.RemoveAt(index);
             T instance = InstanceMap<T>(item);
@@ -58,7 +85,7 @@ namespace RealmdigitalInterview.Core.Implementations
 
         public T GetModel<T>(string command, object id)
         {
-            var item = FindById(id);
+            var item = FindById<T>(id);
             T instance = InstanceMap<T>(item);
 
             return instance;
@@ -66,12 +93,13 @@ namespace RealmdigitalInterview.Core.Implementations
 
         public List<T> GetCollection<T>(string command)
         {
-            return moqCollection.Cast<T>().ToList();
+            return moqCollection.OfType<T>().ToList();
         }
 
         public List<T> GetCollection<T>(string command, object filters)
         {
             var result = new List<T>();
+            var subCollection = moqCollection.OfType<T>().ToList();
 
             var filterProps = filters.GetType().GetProperties();
 
@@ -79,15 +107,16 @@ namespace RealmdigitalInterview.Core.Implementations
             {
                 var filterName = filterProp.Name;
                 var filterValue = filterProp.GetValue(filters);
-                
-                foreach (var item in moqCollection)
+
+                foreach (var item in subCollection)
                 {
                     foreach (var prop in item.GetType().GetProperties())
                     {
                         var propName = prop.Name;
                         var propValue = prop.GetValue(item);
 
-                        if (filterName.ToString() == propName.ToString()) {
+                        if (filterName.ToString() == propName.ToString())
+                        {
                             if (filterValue.ToString() == propValue.ToString())
                             {
                                 T instance = InstanceMap<T>(item);
@@ -97,7 +126,7 @@ namespace RealmdigitalInterview.Core.Implementations
                     }
                 }
             }
-            
+
             return result;
         }
     }
