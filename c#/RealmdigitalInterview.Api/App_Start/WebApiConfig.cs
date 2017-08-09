cef6860919bdbfc096e0747411b040b0ce07b1c2
Thinus Jacobs <marthinuswesseljacobs@gmail.com>
2017-08-09 12:50:05 +0200

    added api versioning and initial product controller

--- c#/RealmdigitalInterview.Api/App_Start/WebApiConfig.cs
+++ c#/RealmdigitalInterview.Api/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Web.Http.Routing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace RealmdigitalInterview.Api
 {
@@ -10,13 +12,20 @@ namespace RealmdigitalInterview.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var constraintResolver = new DefaultInlineConstraintResolver()
+            {
+                ConstraintMap = {
+                    ["apiVersion"] = typeof( ApiVersionRouteConstraint )
+                }
+            };
 
             // Web API routes
-            config.MapHttpAttributeRoutes();
+            config.MapHttpAttributeRoutes(constraintResolver);
+            config.AddApiVersioning();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{apiVersion}/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ c#/RealmdigitalInterview.Api/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RealmdigitalInterview.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [RoutePrefix("api/v{version:apiVersion}/products")]
+    public class ProductController : ApiController
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public HttpResponseMessage Get(int id) {
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
