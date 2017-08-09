22e62ae1e35aedb8e6937e60d2c54902c6817a71
Thinus Jacobs <marthinuswesseljacobs@gmail.com>
2017-08-09 15:17:34 +0200

    added Models project for shared models

---
+++ c#/RealmdigitalInterview.Models/PriceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RealmdigitalInterview.Models.Price
+{
+    public class PriceModel
+    {
+        public int PriceId { get; set; }
+        public string SellingPrice { get; set; }
+        public string CurrencyCode { get; set; }
+    }
+}
