328fc255cae646881471d40918b1b86c2a1375cf
Thinus Jacobs <marthinuswesseljacobs@gmail.com>
2017-08-09 19:46:34 +0200

    added JsonClient wrapper to hide WebClient implementation.

---
+++ c#/Realmdigital Interview/Services/JsonClient/IJsonClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Realmdigital_Interview.Services.JsonClient
+{
+    public interface IJsonClient
+    {
+        TResponse Get<TResponse>(string url);
+        TResponse Post<TResponse>(string url, object parameters);
+    }
+}
---
+++ c#/Realmdigital Interview/Services/JsonClient/JsonClient.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Realmdigital_Interview.Global;
+using System.Net;
+
+namespace Realmdigital_Interview.Services.JsonClient
+{
+    public class JsonClient : IJsonClient
+    {
+        public TResponse Get<TResponse>(string url)
+        {
+            string response = "";
+            
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                response = client.DownloadString(url);
+            }
+            var reponseObject = JsonConvert.DeserializeObject<TResponse>(response);
+
+            return reponseObject;
+        }
+
+        public TResponse Post<TResponse>(string url, object parameters)
+        {
+            string response = "";
+
+            var jsonPars = JsonConvert.SerializeObject(parameters);
+
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                response = client.UploadString(url, "POST", jsonPars);
+            }
+            var reponseObject = JsonConvert.DeserializeObject<TResponse>(response);
+
+            return reponseObject;
+        }
+    }
+}
